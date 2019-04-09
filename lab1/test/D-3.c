8b6b6f9bd322332b987c66932c41bd29f7762c7a
yuanjingsong <532473857@qq.com>
2019-04-09 20:05:30 +0800

    add new test

---
+++ lab1/test/D-3.c
@@ -0,0 +1,25 @@
+/**
+int a, b; 
+**comments
+/\\\\\\\//
+/
+*/
+int /*/**\function\*/ maxFunction(int a, // inty) {
+//parameters: a, b
+int /*z*/ b ) {
+    int z;
+    /****
+    int z = a; 
+    ****\*/
+    if (a > b) { /* {
+            z = 1; a = 3;
+        b = a * 2; /*{ \\\
+        \\
+        */ z = a;
+    }
+    else { // b > a coments *//\\//*
+    //\\*//\\*//\\//\\*//\\*//\\//
+        z = b;
+    }
+    return z /*ends \/\/\/\/*/;
+}
